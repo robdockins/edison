cb93b48bb7b2bd8ef941f3953eae61eeda4393dd
robdockins <robdockins@fastmail.fm>
2006-02-05 13:49:46 -0800

    clean up Data.Edison.Prelude
    darcs-hash:20060205214946-f399b-9201f703e2f6ee4e2b37951ea7e92f61f663c568

--- src/Data/Edison/Prelude.hs
+++ src/Data/Edison/Prelude.hs
@@ -1,35 +1,45 @@
 -- Copyright (c) 1998 Chris Okasaki.  
 -- See COPYRIGHT file for terms and conditions.
 
-module Data.Edison.Prelude where
+-- | This module is a central depository of common definitions
+--   used throughout Edison.
+module Data.Edison.Prelude (
+-- * Hashing classes
+  Hash (..)
+, UniqueHash
+, ReversibleHash (..)
+) where
 
+
+-- | This class represents hashable objects. If obeys the 
+--   following invariant:
+--
+-- @forall x,y :: a. (x == y) implies (hash x == hash y)@
 
 class Eq a => Hash a where
   hash :: a -> Int
-  -- forall x,y :: a. (x == y) implies (hash x == hash y)
+
+
+-- | This class represents hashable objects where the hash funcion
+--   is /unique/ (injective).  There are no new methods, just a 
+--   stronger invariant:
+--
+-- @forall x,y :: a. (x == y) iff (hash x == hash y)@
 
 class Hash a => UniqueHash a
-  -- no new methods, just a stronger invariant
-  -- forall x,y :: a. (x == y) iff (hash x == hash y)
+
+
+-- | This class represents hashable objects where the hash is
+--   reversable.
+--
+-- @forall x :: a. unhash (hash x) == x@
+--
+--  Note that:
+--
+-- @hash (unhash i) == i@
+--
+-- does not necessarily hold because unhash is not necessarily
+-- defined for all i, only for all i in the range of hash.
 
 class UniqueHash a => ReversibleHash a where
   unhash :: Int -> a
-  -- forall x :: a. unhash (hash x) == x
-
-  -- Note that 
-  --   hash (unhash i) == i
-  -- does not necessarily hold because unhash is not necessarily
-  -- defined for all i, only for all i in the range of hash.
-
-
--- Enum types
---
-{-
-instance (Eq a, Enum a) => Hash a where
-  hash = fromEnum
-
-instance (Eq a, Enum a) => UniqueHash a where
-
-instance (Eq a, Enum a) => ReversibleHash a where
-  unhash = toEnum
--}
