0a969e84f17e54c0361b166a52c2263e5e4ac605
gwern0 <gwern0@gmail.com>
2008-01-11 13:51:33 -0800

    -Wall police, LazyPairingHeap.hs
    darcs-hash:20080111215133-f7719-b322e68936bc75614c32b434417f63ac782e1ca1

--- edison-core/src/Data/Edison/Coll/LazyPairingHeap.hs
+++ edison-core/src/Data/Edison/Coll/LazyPairingHeap.hs
@@ -1,6 +1,6 @@
 -- |
 --   Module      :  Data.Edison.Coll.LazyPairingHeap
---   Copyright   :  Copyright (c) 1998-1999 Chris Okasaki
+--   Copyright   :  Copyright (c) 1998-1999, 2008 Chris Okasaki
 --   License     :  MIT; see COPYRIGHT file for terms and conditions
 --
 --   Maintainer  :  robdockins AT fastmail DOT fm
@@ -41,25 +41,25 @@ module Data.Edison.Coll.LazyPairingHeap (
 ) where
 
 import Prelude hiding (null,foldr,foldl,foldr1,foldl1,lookup,filter)
-import Data.Edison.Prelude
 import qualified Data.Edison.Coll as C ( CollX(..), OrdCollX(..),
-				   Coll(..), OrdColl(..), toOrdList )
+                                   Coll(..), OrdColl(..), toOrdList )
 import qualified Data.Edison.Seq as S
 import Data.Edison.Coll.Defaults
-import Data.List(sort)
+import Data.List (sort)
 import Data.Monoid
 import Control.Monad
 import Test.QuickCheck
 
+moduleName :: String
 moduleName = "Data.Edison.Coll.LazyPairingHeap"
 
 
-data Heap a = E 
+data Heap a = E
             | H1 a (Heap a)
             | H2 a !(Heap a) (Heap a)
 
 
--- Invariants: 
+-- Invariants:
 --   * left child of H2 not empty
 structuralInvariant :: Heap a -> Bool
 structuralInvariant E = True
@@ -69,10 +69,11 @@ structuralInvariant (H2 _ l r) = structuralInvariant l && structuralInvariant r
 
 -- second arg is not empty
 -- not used!
-link E h = h
-link (H1 x b) a = H2 x a b
-link (H2 x a b) a' = H1 x (union (union a a') b)
+-- link E h = h
+-- link (H1 x b) a = H2 x a b
+-- link (H2 x a b) a' = H1 x (union (union a a') b)
 
+makeH2 :: a -> Heap a -> Heap a -> Heap a
 makeH2 x E xs = H1 x xs
 makeH2 x h xs = H2 x h xs
 
@@ -93,14 +94,14 @@ insert x h@(H2 y a b)
 
 union :: Ord a => Heap a -> Heap a -> Heap a
 union E h = h
-union hx@(H1 x xs) E = hx
+union hx@(H1 _ _) E = hx
 union hx@(H1 x xs) hy@(H1 y ys)
   | x <= y    = H2 x hy xs
   | otherwise = H2 y hx ys
 union hx@(H1 x xs) hy@(H2 y a ys)
   | x <= y    = H2 x hy xs
   | otherwise = H1 y (union (union hx a) ys)
-union hx@(H2 x a xs) E = hx
+union hx@(H2 _ _ _) E = hx
 union hx@(H2 x a xs) hy@(H1 y ys)
   | x <= y    = H1 x (union (union hy a) xs)
   | otherwise = H2 y hx ys
@@ -115,7 +116,7 @@ delete y h = case del h of Just h' -> h'
         del (H1 x xs) =
           case compare x y of
             LT -> case del xs of
-                    Just xs -> Just (H1 x xs)
+                    Just ys -> Just (H1 x ys)
                     Nothing -> Nothing
             EQ -> Just xs
             GT -> Nothing
@@ -130,7 +131,7 @@ delete y h = case del h of Just h' -> h'
             GT -> Nothing
 
 deleteAll :: Ord a => a -> Heap a -> Heap a
-deleteAll y E = E
+deleteAll _ E = E
 deleteAll y h@(H1 x xs) =
   case compare x y of
     LT -> H1 x (deleteAll y xs)
@@ -147,7 +148,7 @@ deleteSeq = delList . sort . S.toList
   where delList [] h = h
         delList (y:ys) h = del y ys h
 
-        del y ys E = E
+        del _ _ E = E
         del y ys h@(H1 x xs) =
           case compare x y of
             LT -> H1 x (del y ys xs)
@@ -172,11 +173,11 @@ null _ = False
 
 size :: Heap a -> Int
 size E = 0
-size (H1 x xs) = 1 + size xs
-size (H2 x h xs) = 1 + size h + size xs
+size (H1 _ xs) = 1 + size xs
+size (H2 _ h xs) = 1 + size h + size xs
 
 member :: Ord a => a -> Heap a -> Bool
-member x E = False
+member _ E = False
 member x (H1 y ys) =
   case compare x y of
     LT -> False
@@ -189,7 +190,7 @@ member x (H2 y h ys) =
     GT -> member x h || member x ys
 
 count :: Ord a => a -> Heap a -> Int
-count x E = 0
+count _ E = 0
 count x (H1 y ys) =
   case compare x y of
     LT -> 0
@@ -203,8 +204,8 @@ count x (H2 y h ys) =
 
 deleteMin :: Ord a => Heap a -> Heap a
 deleteMin E = E
-deleteMin (H1 x xs) = xs
-deleteMin (H2 x h xs) = union h xs
+deleteMin (H1 _ xs) = xs
+deleteMin (H2 _ h xs) = union h xs
 
 unsafeInsertMin :: Ord a => a -> Heap a -> Heap a
 unsafeInsertMin = H1
@@ -221,7 +222,7 @@ unsafeAppend (H1 x xs) h = H2 x h xs
 unsafeAppend (H2 x a xs) h = H1 x (union (unsafeAppend a h) xs)
 
 filterLT :: Ord a => a -> Heap a -> Heap a
-filterLT y E = E
+filterLT _ E = E
 filterLT y (H1 x xs)
   | x < y = H1 x (filterLT y xs)
   | otherwise = E
@@ -230,7 +231,7 @@ filterLT y (H2 x h xs)
   | otherwise = E
 
 filterLE :: Ord a => a -> Heap a -> Heap a
-filterLE y E = E
+filterLE _ E = E
 filterLE y (H1 x xs)
   | x <= y = H1 x (filterLE y xs)
   | otherwise = E
@@ -241,25 +242,25 @@ filterLE y (H2 x h xs)
 filterGT :: Ord a => a -> Heap a -> Heap a
 filterGT y h = fgt h E
   where fgt E rest = rest
-        fgt h@(H1 x xs) rest
-          | x > y = union h rest
+        fgt i@(H1 x xs) rest
+          | x > y = union i rest
           | otherwise = fgt xs rest
-        fgt h@(H2 x a xs) rest
-          | x > y = union h rest
+        fgt i@(H2 x a xs) rest
+          | x > y = union i rest
           | otherwise = fgt a (fgt xs rest)
 
 filterGE :: Ord a => a -> Heap a -> Heap a
 filterGE y h = fge h E
   where fge E rest = rest
-        fge h@(H1 x xs) rest
-          | x >= y = union h rest
+        fge i@(H1 x xs) rest
+          | x >= y = union i rest
           | otherwise = fge xs rest
-        fge h@(H2 x a xs) rest
-          | x >= y = union h rest
+        fge i@(H2 x a xs) rest
+          | x >= y = union i rest
           | otherwise = fge a (fge xs rest)
 
 partitionLT_GE :: Ord a => a -> Heap a -> (Heap a, Heap a)
-partitionLT_GE y E = (E,E)
+partitionLT_GE _ E = (E,E)
 partitionLT_GE y h@(H1 x xs)
   | x < y = let (xs',xs'') = partitionLT_GE y xs
             in (H1 x xs',xs'')
@@ -271,7 +272,7 @@ partitionLT_GE y h@(H2 x a xs)
   | otherwise = (E, h)
 
 partitionLE_GT :: Ord a => a -> Heap a -> (Heap a, Heap a)
-partitionLE_GT y E = (E,E)
+partitionLE_GT _ E = (E,E)
 partitionLE_GT y h@(H1 x xs)
   | x <= y = let (xs',xs'') = partitionLE_GT y xs
              in (H1 x xs',xs'')
@@ -283,7 +284,7 @@ partitionLE_GT y h@(H2 x a xs)
   | otherwise = (E, h)
 
 partitionLT_GT :: Ord a => a -> Heap a -> (Heap a, Heap a)
-partitionLT_GT y E = (E,E)
+partitionLT_GT _ E = (E,E)
 partitionLT_GT y h@(H1 x xs) =
   case compare x y of
     LT -> let (xs',xs'') = partitionLT_GT y xs
@@ -302,39 +303,39 @@ toSeq :: S.Sequence seq => Heap a -> seq a
 toSeq h = tol h S.empty
   where tol E rest = rest
         tol (H1 x xs) rest = S.lcons x (tol xs rest)
-        tol (H2 x h xs) rest = S.lcons x (tol h (tol xs rest))
+        tol (H2 x i xs) rest = S.lcons x $ tol i $ tol xs rest
 
 fold :: (a -> b -> b) -> b -> Heap a -> b
-fold f c E = c
+fold _ c E = c
 fold f c (H1 x xs) = f x (fold f c xs)
 fold f c (H2 x h xs) = f x (fold f (fold f c xs) h)
 
 fold' :: (a -> b -> b) -> b -> Heap a -> b
-fold' f c E = c
+fold' _ c E = c
 fold' f c (H1 x xs)   = c `seq` f x $! (fold' f c xs)
 fold' f c (H2 x h xs) = c `seq` f x $! (fold' f (fold' f c xs) h)
 
 
 fold1 :: (a -> a -> a) -> Heap a -> a
-fold1 f E = error "LazyPairingHeap.fold1: empty heap"
+fold1 _ E = error "LazyPairingHeap.fold1: empty heap"
 fold1 f (H1 x xs) = fold f x xs
 fold1 f (H2 x h xs) = fold f (fold f x xs) h
 
 fold1' :: (a -> a -> a) -> Heap a -> a
-fold1' f E = error "LazyPairingHeap.fold1': empty heap"
+fold1' _ E = error "LazyPairingHeap.fold1': empty heap"
 fold1' f (H1 x xs)   = fold' f x xs
 fold1' f (H2 x h xs) = fold' f (fold' f x xs) h
 
 
 filter :: Ord a => (a -> Bool) -> Heap a -> Heap a
-filter p E = E
+filter _ E = E
 filter p (H1 x xs) = if p x then H1 x (filter p xs) else filter p xs
 filter p (H2 x h xs) =
   if p x then makeH2 x (filter p h) (filter p xs)
          else union (filter p h) (filter p xs)
 
 partition :: Ord a => (a -> Bool) -> Heap a -> (Heap a, Heap a)
-partition p E = (E, E)
+partition _ E = (E, E)
 partition p (H1 x xs) = if p x then (H1 x xs',xs'') else (xs',H1 x xs'')
     where (xs',xs'') = partition p xs
 partition p (H2 x h xs) =
@@ -351,10 +352,10 @@ lookupAll y h = look h S.empty
             LT -> look xs rest
             EQ -> S.lcons x (look xs rest)
             GT -> rest
-        look (H2 x h xs) rest =
+        look (H2 x i xs) rest =
           case compare x y of
-            LT -> look h (look xs rest)
-            EQ -> S.lcons x (look h (look xs rest))
+            LT -> look i $ look xs rest
+            EQ -> S.lcons x $ look i $ look xs rest
             GT -> rest
 
 minView :: (Ord a, Monad m) => Heap a -> m (a, Heap a)
@@ -364,8 +365,8 @@ minView (H2 x h xs) = return (x,union h xs)
 
 minElem :: Heap a -> a
 minElem E = error "LazyPairingHeap.minElem: empty heap"
-minElem (H1 x xs) = x
-minElem (H2 x h xs) = x
+minElem (H1 x _) = x
+minElem (H2 x _ _) = x
 
 maxView :: (Ord a, Monad m) => Heap a -> m (a, Heap a)
 maxView E = fail "LazyPairingHeap.maxView: empty heap"
@@ -373,12 +374,13 @@ maxView xs = return (y,xs')
   where (xs', y) = maxView' xs
 
 -- not exported
+maxView' :: (Ord a) => Heap a -> (Heap a, a)
 maxView' (H1 x E) = (E, x)
 maxView' (H1 x xs) = (H1 x xs', y)
   where (xs', y) = maxView' xs
 maxView' (H2 x a E) = (H1 x a', y)
   where (a', y) = maxView' a
-maxView' (H2 x a xs) = 
+maxView' (H2 x a xs) =
     if y > z then (makeH2 x a' xs, y) else (H2 x a xs', z)
   where (a', y) = maxView' a
         (xs', z) = maxView' xs
@@ -387,66 +389,66 @@ maxView' E = error "LazyPairingHeap.maxView': bug!"
 maxElem :: Ord a => Heap a -> a
 maxElem E = error "LazyPairingHeap.maxElem: empty heap"
 maxElem (H1 x E) = x
-maxElem (H1 x xs) = maxElem xs
-maxElem (H2 x h E) = maxElem h
-maxElem (H2 x h xs) = max (maxElem h) (maxElem xs)
+maxElem (H1 _ xs) = maxElem xs
+maxElem (H2 _ h E) = maxElem h
+maxElem (H2 _ h xs) = max (maxElem h) (maxElem xs)
 
 foldr :: Ord a => (a -> b -> b) -> b -> Heap a -> b
-foldr f c E = c
+foldr _ c E = c
 foldr f c (H1 x xs) = f x (foldr f c xs)
 foldr f c (H2 x h xs) = f x (foldr f c (union h xs))
 
 foldr' :: Ord a => (a -> b -> b) -> b -> Heap a -> b
-foldr' f c E = c
+foldr' _ c E = c
 foldr' f c (H1 x xs)   = c `seq` f x $! (foldr' f c xs)
 foldr' f c (H2 x h xs) = c `seq` f x $! (foldr' f c (union h xs))
 
 foldl :: Ord a => (b -> a -> b) -> b -> Heap a -> b
-foldl f c E = c
+foldl _ c E = c
 foldl f c (H1 x xs) = foldl f (f c x) xs
 foldl f c (H2 x h xs) = foldl f (f c x) (union h xs)
 
 foldl' :: Ord a => (b -> a -> b) -> b -> Heap a -> b
-foldl' f c E = c
+foldl' _ c E = c
 foldl' f c (H1 x xs)   = c `seq` foldl' f (f c x) xs
 foldl' f c (H2 x h xs) = c `seq` foldl' f (f c x) (union h xs)
 
 foldr1 :: Ord a => (a -> a -> a) -> Heap a -> a
-foldr1 f E = error "LazyPairingHeap.foldr1: empty heap"
-foldr1 f (H1 x E) = x
+foldr1 _ E = error "LazyPairingHeap.foldr1: empty heap"
+foldr1 _ (H1 x E) = x
 foldr1 f (H1 x xs) = f x (foldr1 f xs)
 foldr1 f (H2 x h xs) = f x (foldr1 f (union h xs))
 
 foldr1' :: Ord a => (a -> a -> a) -> Heap a -> a
-foldr1' f E = error "LazyPairingHeap.foldr1': empty heap"
-foldr1' f (H1 x E)    = x
+foldr1' _ E = error "LazyPairingHeap.foldr1': empty heap"
+foldr1' _ (H1 x E)    = x
 foldr1' f (H1 x xs)   = f x $! (foldr1' f xs)
 foldr1' f (H2 x h xs) = f x $! (foldr1' f (union h xs))
 
 foldl1 :: Ord a => (a -> a -> a) -> Heap a -> a
-foldl1 f E = error "LazyPairingHeap.foldl1: empty heap"
+foldl1 _ E = error "LazyPairingHeap.foldl1: empty heap"
 foldl1 f (H1 x xs) = foldl f x xs
 foldl1 f (H2 x h xs) = foldl f x (union h xs)
 
 foldl1' :: Ord a => (a -> a -> a) -> Heap a -> a
-foldl1' f E = error "LazyPairingHeap.foldl1': empty heap"
+foldl1' _ E = error "LazyPairingHeap.foldl1': empty heap"
 foldl1' f (H1 x xs)   = foldl' f x xs
 foldl1' f (H2 x h xs) = foldl' f x (union h xs)
 
 unsafeMapMonotonic :: (Ord a,Ord b) => (a -> b) -> Heap a -> Heap b
 unsafeMapMonotonic = mapm
-  where mapm f E = E
+  where mapm _ E = E
         mapm f (H1 x xs) = H1 (f x) (mapm f xs)
         mapm f (H2 x h xs) = H2 (f x) (mapm f h) (mapm f xs)
 
 
 strict :: Heap a -> Heap a
 strict h@E = h
-strict h@(H1 x xs) = strict xs `seq` h
-strict h@(H2 x h' xs) = strict h' `seq` strict xs `seq` h
+strict h@(H1 _ xs) = strict xs `seq` h
+strict h@(H2 _ h' xs) = strict h' `seq` strict xs `seq` h
 
 strictWith :: (a -> b) -> Heap a -> Heap a
-strictWith f h@E = h
+strictWith _ h@E = h
 strictWith f h@(H1 x xs) = f x `seq` strictWith f xs `seq` h
 strictWith f h@(H2 x h' xs) = f x `seq` strictWith f h' `seq` strictWith f xs `seq` h
 
@@ -484,29 +486,29 @@ toOrdSeq = toOrdSeqUsingFoldr
 
 instance Ord a => C.CollX (Heap a) a where
   {singleton = singleton; fromSeq = fromSeq; insert = insert;
-   insertSeq = insertSeq; unionSeq = unionSeq; 
+   insertSeq = insertSeq; unionSeq = unionSeq;
    delete = delete; deleteAll = deleteAll; deleteSeq = deleteSeq;
    null = null; size = size; member = member; count = count;
    strict = strict;
-   structuralInvariant = structuralInvariant; instanceName c = moduleName}
+   structuralInvariant = structuralInvariant; instanceName _ = moduleName}
 
 instance Ord a => C.OrdCollX (Heap a) a where
-  {deleteMin = deleteMin; deleteMax = deleteMax; 
-   unsafeInsertMin = unsafeInsertMin; unsafeInsertMax = unsafeInsertMax; 
-   unsafeFromOrdSeq = unsafeFromOrdSeq; unsafeAppend = unsafeAppend; 
-   filterLT = filterLT; filterLE = filterLE; filterGT = filterGT; 
-   filterGE = filterGE; partitionLT_GE = partitionLT_GE; 
+  {deleteMin = deleteMin; deleteMax = deleteMax;
+   unsafeInsertMin = unsafeInsertMin; unsafeInsertMax = unsafeInsertMax;
+   unsafeFromOrdSeq = unsafeFromOrdSeq; unsafeAppend = unsafeAppend;
+   filterLT = filterLT; filterLE = filterLE; filterGT = filterGT;
+   filterGE = filterGE; partitionLT_GE = partitionLT_GE;
    partitionLE_GT = partitionLE_GT; partitionLT_GT = partitionLT_GT}
 
 instance Ord a => C.Coll (Heap a) a where
-  {toSeq = toSeq; lookup = lookup; lookupM = lookupM; 
-   lookupAll = lookupAll; lookupWithDefault = lookupWithDefault; 
+  {toSeq = toSeq; lookup = lookup; lookupM = lookupM;
+   lookupAll = lookupAll; lookupWithDefault = lookupWithDefault;
    fold = fold; fold' = fold'; fold1 = fold1; fold1' = fold1';
    filter = filter; partition = partition; strictWith = strictWith}
 
 instance Ord a => C.OrdColl (Heap a) a where
-  {minView = minView; minElem = minElem; maxView = maxView; 
-   maxElem = maxElem; foldr = foldr; foldr' = foldr'; 
+  {minView = minView; minElem = minElem; maxView = maxView;
+   maxElem = maxElem; foldr = foldr; foldr' = foldr';
    foldl = foldl; foldl' = foldl'; foldr1 = foldr1;
    foldr1' = foldr1'; foldl1 = foldl1; foldl1' = foldl1';
    toOrdSeq = toOrdSeq; unsafeMapMonotonic = unsafeMapMonotonic}
@@ -539,14 +541,14 @@ instance (Ord a, Arbitrary a) => Arbitrary (Heap a) where
                   mb = minElem b
 
           sift1 x E = H1 x E
-          sift1 x a 
+          sift1 x a
               | x <= ma   = H1 x a
               | otherwise = H1 ma (siftInto x a)
             where ma = minElem a
 
           siftInto x (H1 _ a) = sift1 x a
           siftInto x (H2 _ a b) = sift x a b
-          siftInto x E = error "LazyPairingHeap.arbitrary: bug!"
+          siftInto _ E = error "LazyPairingHeap.arbitrary: bug!"
 
   coarbitrary E = variant 0
   coarbitrary (H1 x a) = variant 1 . coarbitrary x . coarbitrary a
