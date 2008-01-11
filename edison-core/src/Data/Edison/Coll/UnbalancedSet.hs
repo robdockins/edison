f1bf1c7ca21584682b618e31a7bf11d7e07ebcd6
gwern0 <gwern0@gmail.com>
2008-01-11 14:05:06 -0800

    -Wall police, UnbalancedSet.hs
    darcs-hash:20080111220506-f7719-c2144ea7555b049d247ee25b3c0670371965503c

--- edison-core/src/Data/Edison/Coll/UnbalancedSet.hs
+++ edison-core/src/Data/Edison/Coll/UnbalancedSet.hs
@@ -1,6 +1,6 @@
 -- |
 --   Module      :  Data.Edison.Coll.UnbalancedSet
---   Copyright   :  Copyright (c) 1998-1999 Chris Okasaki
+--   Copyright   :  Copyright (c) 1998-1999, 2008 Chris Okasaki
 --   License     :  MIT; see COPYRIGHT file for terms and conditions
 --
 --   Maintainer  :  robdockins AT fastmail DOT fm
@@ -43,10 +43,8 @@ module Data.Edison.Coll.UnbalancedSet (
 
 import Prelude hiding (null,foldr,foldl,foldr1,foldl1,lookup,filter)
 import qualified Prelude
-import Data.Edison.Prelude
 import qualified Data.Edison.Coll as C
 import qualified Data.Edison.Seq as S
-import qualified Data.Edison.Seq.ListSeq as L
 import Data.Edison.Coll.Defaults
 import Data.Monoid
 import Test.QuickCheck
@@ -135,7 +133,7 @@ data Set a = E | T (Set a) a (Set a)
 structuralInvariant :: Ord a => Set a -> Bool
 structuralInvariant t = bounded Nothing Nothing t
    where bounded _ _ E = True
-         bounded lo hi (T l x r)  = cmp_l lo x 
+         bounded lo hi (T l x r)  = cmp_l lo x
                                  && cmp_r x hi
                                  && bounded lo (Just x) l
                                  && bounded (Just x) hi r
@@ -159,7 +157,7 @@ insertWith c x = ins
             EQ -> T a (c x y) b
             GT -> T a y (ins b)
 
-delete x E = E
+delete _ E = E
 delete x (T a y b) =
   case compare x y of
     LT -> T (delete x a) y b
@@ -171,86 +169,86 @@ null (T _ _ _) = False
 
 size t = sz t 0
   where sz E i = i
-        sz (T a x b) i = sz a (sz b (i+1))
+        sz (T a _ b) i = sz a (sz b (i+1))
 
-member x E = False
+member _ E = False
 member x (T a y b) =
   case compare x y of
     LT -> member x a
     EQ -> True
     GT -> member x b
 
-lookupM x E = fail "UnbalancedSet.lookupM: XXX"
+lookupM _ E = fail "UnbalancedSet.lookupM: XXX"
 lookupM x (T a y b) =
   case compare x y of
     LT -> lookupM x a
     EQ -> return y
     GT -> lookupM x b
 
-fold f e E = e
+fold _ e E = e
 fold f e (T a x b) = f x (fold f (fold f e a) b)
 
-fold' f e E = e
+fold' _ e E = e
 fold' f e (T a x b) = e `seq` f x $! (fold' f (fold' f e a) b)
 
-fold1 f E = error "UnbalancedSet.fold1: empty collection"
+fold1 _ E = error "UnbalancedSet.fold1: empty collection"
 fold1 f (T a x b) = fold f (fold f x a) b
 
-fold1' f E = error "UnbalancedSet.fold1': empty collection"
+fold1' _ E = error "UnbalancedSet.fold1': empty collection"
 fold1' f (T a x b) = fold' f (fold' f x a) b
 
 deleteMin E = E
-deleteMin (T E x b) = b
+deleteMin (T E _ b) = b
 deleteMin (T a x b) = T (deleteMin a) x b
 
 deleteMax E = E
-deleteMax (T a x E) = a
+deleteMax (T a _ E) = a
 deleteMax (T a x b) = T a x (deleteMax b)
 
 unsafeInsertMin x t = T E x t
 unsafeInsertMax x t = T t x E
 
 unsafeFromOrdSeq xs = fst (ins xs (S.size xs))
-  where ins xs 0 = (E,xs)
-        ins xs n = let m = n `div` 2
-                       (a,xs') = ins xs m
-                       Just (x,xs'') = S.lview xs'
-                       (b,xs''') = ins xs'' (n - m - 1)
-                   in (T a x b,xs''')
+  where ins ys 0 = (E,ys)
+        ins ys n = let m = n `div` 2
+                       (a,ys') = ins ys m
+                       Just (y,ys'') = S.lview ys'
+                       (b,ys''') = ins ys'' (n - m - 1)
+                   in (T a y b,ys''')
 
 unsafeAppend a b = case minView b of
                      Nothing -> a
                      Just (x,b') -> T a x b'
 
-filterLT y E = E
+filterLT _ E = E
 filterLT y (T a x b) =
   case compare x y of
     LT -> T a x (filterLT y b)
     EQ -> a
     GT -> filterLT y a
 
-filterLE y E = E
+filterLE _ E = E
 filterLE y (T a x b) =
   case compare x y of
     LT -> T a x (filterLE y b)
     EQ -> T a x E
     GT -> filterLE y a
 
-filterGT y E = E
+filterGT _ E = E
 filterGT y (T a x b) =
   case compare x y of
     LT -> filterGT y b
     EQ -> b
     GT -> T (filterGT y a) x b
 
-filterGE y E = E
+filterGE _ E = E
 filterGE y (T a x b) =
   case compare x y of
     LT -> filterGE y b
     EQ -> T E x b
     GT -> T (filterGE y a) x b
 
-partitionLT_GE y E = (E,E)
+partitionLT_GE _ E = (E,E)
 partitionLT_GE y (T a x b) =
   case compare x y of
     LT -> (T a x b0,b1)
@@ -259,7 +257,7 @@ partitionLT_GE y (T a x b) =
     GT -> (a0,T a1 x b)
           where (a0,a1) = partitionLT_GE y a
 
-partitionLE_GT y E = (E,E)
+partitionLE_GT _ E = (E,E)
 partitionLE_GT y (T a x b) =
   case compare x y of
     LT -> (T a x b0,b1)
@@ -268,7 +266,7 @@ partitionLE_GT y (T a x b) =
     GT -> (a0,T a1 x b)
           where (a0,a1) = partitionLE_GT y a
 
-partitionLT_GT y E = (E,E)
+partitionLT_GT _ E = (E,E)
 partitionLT_GT y (T a x b) =
   case compare x y of
     LT -> (T a x b0,b1)
@@ -283,8 +281,8 @@ minView (T a x b) = return (y, T a' x b)
   where Just (y,a') = minView a
 
 minElem E = error "UnbalancedSet.minElem: empty collection"
-minElem (T E x b) = x
-minElem (T a x b) = minElem a
+minElem (T E x _) = x
+minElem (T a _ _) = minElem a
 
 maxView E = fail "UnbalancedSet.maxView: empty collection"
 maxView (T a x E) = return (x, a)
@@ -292,45 +290,45 @@ maxView (T a x b) = return (y, T a x b')
   where Just (y, b') = maxView b
 
 maxElem E = error "UnbalancedSet.maxElem: empty collection"
-maxElem (T a x E) = x
-maxElem (T a x b) = maxElem b
+maxElem (T _ x E) = x
+maxElem (T _ _ b) = maxElem b
 
-foldr f e E = e
+foldr _ e E = e
 foldr f e (T a x b) = foldr f (f x (foldr f e b)) a
 
-foldr' f e E = e
+foldr' _ e E = e
 foldr' f e (T a x b) = e `seq` foldr' f (f x $! (foldr' f e b)) a
 
-foldl f e E = e
+foldl _ e E = e
 foldl f e (T a x b) = foldl f (f (foldl f e a) x) b
 
-foldl' f e E = e
+foldl' _ e E = e
 foldl' f e (T a x b) = e `seq` foldl' f ((f $! (foldl' f e a)) x) b
 
-foldr1 f E = error "UnbalancedSet.foldr1: empty collection"
+foldr1 _ E = error "UnbalancedSet.foldr1: empty collection"
 foldr1 f (T a x E) = foldr f x a
 foldr1 f (T a x b) = foldr f (f x (foldr1 f b)) a
 
-foldr1' f E = error "UnbalancedSet.foldr1': empty collection"
+foldr1' _ E = error "UnbalancedSet.foldr1': empty collection"
 foldr1' f (T a x E) = foldr' f x a
 foldr1' f (T a x b) = foldr' f (f x $! (foldr1' f b)) a
 
-foldl1 f E = error "UnbalancedSet.foldl1: empty collection"
+foldl1 _ E = error "UnbalancedSet.foldl1: empty collection"
 foldl1 f (T E x b) = foldl f x b
 foldl1 f (T a x b) = foldl f (f (foldl1 f a) x) b
 
-foldl1' f E = error "UnbalancedSet.foldl1': empty collection"
+foldl1' _ E = error "UnbalancedSet.foldl1': empty collection"
 foldl1' f (T E x b) = foldl' f x b
 foldl1' f (T a x b) = foldl' f ((f $! (foldl1' f a)) x) b
 
-unsafeMapMonotonic f E = E
-unsafeMapMonotonic f (T a x b) = 
+unsafeMapMonotonic _ E = E
+unsafeMapMonotonic f (T a x b) =
     T (unsafeMapMonotonic f a) (f x) (unsafeMapMonotonic f b)
 
 strict s@E = s
-strict s@(T l x r) = strict l `seq` strict r `seq` s
+strict s@(T l _ r) = strict l `seq` strict r `seq` s
 
-strictWith f s@E = s
+strictWith _ s@E = s
 strictWith f s@(T l x r) = f x `seq` strictWith f l `seq` strictWith f r `seq` s
 
 -- the remaining functions all use default definitions
@@ -369,29 +367,29 @@ intersectionWith = intersectionWithUsingOrdLists
 
 instance Ord a => C.CollX (Set a) a where
   {singleton = singleton; fromSeq = fromSeq; insert = insert;
-   insertSeq = insertSeq; unionSeq = unionSeq; 
+   insertSeq = insertSeq; unionSeq = unionSeq;
    delete = delete; deleteAll = deleteAll; deleteSeq = deleteSeq;
    null = null; size = size; member = member; count = count;
    strict = strict;
-   structuralInvariant = structuralInvariant; instanceName c = moduleName}
+   structuralInvariant = structuralInvariant; instanceName _ = moduleName}
 
 instance Ord a => C.OrdCollX (Set a) a where
-  {deleteMin = deleteMin; deleteMax = deleteMax; 
-   unsafeInsertMin = unsafeInsertMin; unsafeInsertMax = unsafeInsertMax; 
-   unsafeFromOrdSeq = unsafeFromOrdSeq; unsafeAppend = unsafeAppend; 
-   filterLT = filterLT; filterLE = filterLE; filterGT = filterGT; 
-   filterGE = filterGE; partitionLT_GE = partitionLT_GE; 
+  {deleteMin = deleteMin; deleteMax = deleteMax;
+   unsafeInsertMin = unsafeInsertMin; unsafeInsertMax = unsafeInsertMax;
+   unsafeFromOrdSeq = unsafeFromOrdSeq; unsafeAppend = unsafeAppend;
+   filterLT = filterLT; filterLE = filterLE; filterGT = filterGT;
+   filterGE = filterGE; partitionLT_GE = partitionLT_GE;
    partitionLE_GT = partitionLE_GT; partitionLT_GT = partitionLT_GT}
 
 instance Ord a => C.Coll (Set a) a where
-  {toSeq = toSeq; lookup = lookup; lookupM = lookupM; 
-   lookupAll = lookupAll; lookupWithDefault = lookupWithDefault; 
+  {toSeq = toSeq; lookup = lookup; lookupM = lookupM;
+   lookupAll = lookupAll; lookupWithDefault = lookupWithDefault;
    fold = fold; fold' = fold'; fold1 = fold1; fold1' = fold1';
    filter = filter; partition = partition; strictWith = strictWith}
 
 instance Ord a => C.OrdColl (Set a) a where
-  {minView = minView; minElem = minElem; maxView = maxView; 
-   maxElem = maxElem; foldr = foldr; foldr' = foldr'; 
+  {minView = minView; minElem = minElem; maxView = maxView;
+   maxElem = maxElem; foldr = foldr; foldr' = foldr';
    foldl = foldl; foldl' = foldl'; foldr1 = foldr1; foldr1' = foldr1';
    foldl1 = foldl1; foldl1' = foldl1'; toOrdSeq = toOrdSeq;
    unsafeMapMonotonic = unsafeMapMonotonic}
@@ -402,7 +400,7 @@ instance Ord a => C.SetX (Set a) a where
    properSubset = properSubset; subset = subset}
 
 instance Ord a => C.Set (Set a) a where
-  {fromSeqWith = fromSeqWith; insertWith = insertWith; 
+  {fromSeqWith = fromSeqWith; insertWith = insertWith;
    insertSeqWith = insertSeqWith; unionl = unionl; unionr = unionr;
    unionWith = unionWith; unionSeqWith = unionSeqWith;
    intersectionWith = intersectionWith}
@@ -427,7 +425,7 @@ instance (Ord a, Arbitrary a) => Arbitrary (Set a) where
                  return (Prelude.foldr insert empty xs)
 
   coarbitrary E = variant 0
-  coarbitrary (T a x b) = 
+  coarbitrary (T a x b) =
     variant 1 . coarbitrary a . coarbitrary x . coarbitrary b
 
 instance (Ord a) => Monoid (Set a) where
