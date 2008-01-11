757a34a07fb0cebe6c3a66016e1017c339db0ab3
gwern0 <gwern0@gmail.com>
2008-01-11 13:59:14 -0800

    partial -Wall police: LeftistHeap.hs
    darcs-hash:20080111215914-f7719-de53be49a6dbfeaad8c335e154cbc6492caf4745

--- edison-core/src/Data/Edison/Coll/LeftistHeap.hs
+++ edison-core/src/Data/Edison/Coll/LeftistHeap.hs
@@ -1,6 +1,6 @@
 -- |
 --   Module      :  Data.Edison.Coll.LeftistHeap
---   Copyright   :  Copyright (c) 1998-1999 Chris Okasaki
+--   Copyright   :  Copyright (c) 1998-1999, 2008 Chris Okasaki
 --   License     :  MIT; see COPYRIGHT file for terms and conditions
 --
 --   Maintainer  :  robdockins AT fastmail DOT fm
@@ -40,8 +40,7 @@ module Data.Edison.Coll.LeftistHeap (
 ) where
 
 import Prelude hiding (null,foldr,foldl,foldr1,foldl1,lookup,filter)
-import Data.Edison.Prelude
-import qualified Data.Edison.Coll as C ( CollX(..), OrdCollX(..), Coll(..), OrdColl(..), 
+import qualified Data.Edison.Coll as C ( CollX(..), OrdCollX(..), Coll(..), OrdColl(..),
                                    unionList, toOrdList )
 import qualified Data.Edison.Seq as S
 import Data.Edison.Coll.Defaults
@@ -49,6 +48,7 @@ import Data.Monoid
 import Control.Monad
 import Test.QuickCheck
 
+moduleName :: String
 moduleName = "Data.Edison.Coll.LeftistHeap"
 
 data Heap a = E | L !Int !a !(Heap a) !(Heap a)
@@ -61,20 +61,21 @@ data Heap a = E | L !Int !a !(Heap a) !(Heap a)
 
 structuralInvariant :: Ord a => Heap a -> Bool
 structuralInvariant E = True
-structuralInvariant t@(L i x l r) =
+structuralInvariant t@(L i x _ _) =
     i == rank t && isMin x t && checkLeftist t
 
- where rank E = 0
-       rank (L _ _ _ r) = (rank r) + 1
+ where rank :: Heap a -> Int
+       rank E = 0
+       rank (L _ _ _ s) = (rank s) + 1
 
-       isMin x E = True
-       isMin x (L _ y l r) = x <= y && (isMin y l) && (isMin y r)
+       isMin _ E = True
+       isMin z (L _ y l r) = z <= y && (isMin y l) && (isMin y r)
 
        checkLeftist E = True
-       checkLeftist t@(L i _ l r) =
+       checkLeftist (L _ _ l r) =
           rank l >= rank r && checkLeftist l && checkLeftist r
 
-
+node :: a -> Heap a -> Heap a -> Heap a
 node x a E = L 1 x a E
 node x E b = L 1 x b E
 node x a@(L m _ _ _) b@(L n _ _ _)
@@ -97,17 +98,17 @@ singleton x = L 1 x E E
 
 insert :: Ord a => a -> Heap a -> Heap a
 insert x E = L 1 x E E
-insert x h@(L m y a b)
+insert x h@(L _ y a b)
   | x <= y    = L 1 x h E
   | otherwise = node y a (insert x b)
 
 union :: Ord a => Heap a -> Heap a -> Heap a
 union E h = h
 union h@(L _ x a b) h' = union' h x a b h'
-  where union' h x a b E = h
-        union' hx x a b hy@(L _ y c d)
-          | x <= y    = node x a (union' hy y c d b)
-          | otherwise = node y c (union' hx x a b d)
+  where union' i _ _ _ E = i
+        union' hx z q e hy@(L _ y c d)
+          | z <= y    = node z q (union' hy y c d e)
+          | otherwise = node y c (union' hx z q e d)
 
 {-
 union E h = h
@@ -139,7 +140,7 @@ deleteAll x h@(L _ y a b) =
     LT -> h
     EQ -> union (deleteAll x a) (deleteAll x b)
     GT -> node y (deleteAll x a) (deleteAll x b)
-deleteAll x E = E
+deleteAll _ E = E
 
 null :: Ord a => Heap a -> Bool
 null E = True
@@ -151,7 +152,7 @@ size h = sz h 0
         sz (L _ _ a b) i = sz a (sz b (i + 1))
 
 member :: Ord a => a -> Heap a -> Bool
-member x E = False
+member _ E = False
 member x (L _ y a b) =
   case compare x y of
     LT -> False
@@ -159,7 +160,7 @@ member x (L _ y a b) =
     GT -> member x b || member x a
 
 count :: Ord a => a -> Heap a -> Int
-count x E = 0
+count _ E = 0
 count x (L _ y a b) =
   case compare x y of
     LT -> 0
@@ -172,14 +173,14 @@ toSeq h = tol h S.empty
         tol (L _ x a b) rest = S.lcons x (tol b (tol a rest))
 
 lookupM :: (Ord a, Monad m) => a -> Heap a -> m a
-lookupM x E = fail "LeftistHeap.lookupM: XXX"
+lookupM _ E = fail "LeftistHeap.lookupM: XXX"
 lookupM x (L _ y a b) =
   case compare x y of
     LT -> fail "LeftistHeap.lookupM: XXX"
     EQ -> return y
     GT -> case lookupM x b `mplus` lookupM x a of
                 Nothing -> fail "LeftistHeap.lookupM: XXX"
-                Just x  -> return x
+                Just q -> return q
 
 lookupAll :: (Ord a,S.Sequence seq) => a -> Heap a -> seq a
 lookupAll x h = look h S.empty
@@ -191,30 +192,30 @@ lookupAll x h = look h S.empty
             GT -> look b (look a ys)
 
 fold :: Ord a => (a -> b -> b) -> b -> Heap a -> b
-fold f e E = e
+fold _ e E = e
 fold f e (L _ x a b) = f x (fold f (fold f e a) b)
 
 fold' :: Ord a => (a -> b -> b) -> b -> Heap a -> b
-fold' f e E = e
+fold' _ e E = e
 fold' f e (L _ x a b) = e `seq` f x $! (fold' f (fold' f e a) b)
 
 fold1 :: Ord a => (a -> a -> a) -> Heap a -> a
-fold1 f E = error "LeftistHeap.fold1: empty collection"
+fold1 _ E = error "LeftistHeap.fold1: empty collection"
 fold1 f (L _ x a b) = fold f (fold f x a) b
 
 fold1' :: Ord a => (a -> a -> a) -> Heap a -> a
-fold1' f E = error "LeftistHeap.fold1': empty collection"
+fold1' _ E = error "LeftistHeap.fold1': empty collection"
 fold1' f (L _ x a b) = fold' f (fold' f x a) b
 
 
 filter :: Ord a => (a -> Bool) -> Heap a -> Heap a
-filter p E = E
+filter _ E = E
 filter p (L _ x a b)
     | p x = node x (filter p a) (filter p b)
     | otherwise = union (filter p a) (filter p b)
 
 partition :: Ord a => (a -> Bool) -> Heap a -> (Heap a, Heap a)
-partition p E = (E, E)
+partition _ E = (E, E)
 partition p (L _ x a b)
     | p x = (node x a' b', union a'' b'')
     | otherwise = (union a' b', node x a'' b'')
@@ -224,12 +225,12 @@ partition p (L _ x a b)
 
 deleteMin :: Ord a => Heap a -> Heap a
 deleteMin E = E
-deleteMin (L _ x a b) = union a b
+deleteMin (L _ _ a b) = union a b
 
 deleteMax :: Ord a => Heap a -> Heap a
 deleteMax h = case maxView h of
                 Nothing     -> E
-                Just (x,h') -> h'
+                Just (_,h') -> h'
 
 unsafeInsertMin :: Ord a => a -> Heap a -> Heap a
 unsafeInsertMin x h = L 1 x h E
@@ -240,11 +241,11 @@ unsafeAppend (L _ y a b) h = node y a (unsafeAppend b h)
 
 filterLT :: Ord a => a -> Heap a -> Heap a
 filterLT y (L _ x a b) | x < y = node x (filterLT y a) (filterLT y b)
-filterLT y _ = E
+filterLT _ _ = E
 
 filterLE :: Ord a => a -> Heap a -> Heap a
 filterLE y (L _ x a b) | x <= y = node x (filterLE y a) (filterLE y b)
-filterLE y _ = E
+filterLE _ _ = E
 
 filterGT :: Ord a => a -> Heap a -> Heap a
 filterGT y h = C.unionList (collect h [])
@@ -287,13 +288,13 @@ partitionLT_GT y h = (h', C.unionList hs)
   where (h', hs) = collect h []
 
         collect E hs = (E, hs)
-        collect h@(L _ x a b) hs = 
+        collect h@(L _ x a b) is =
           case compare x y of
-            GT -> (E, h:hs)
-            EQ -> let (a', hs') = collect a hs
+            GT -> (E, h:is)
+            EQ -> let (a', hs') = collect a is
                       (b', hs'') = collect b hs'
                   in (union a' b', hs'')
-            LT -> let (a', hs') = collect a hs
+            LT -> let (a', hs') = collect a is
                       (b', hs'') = collect b hs'
                   in (node x a' b', hs'')
 
@@ -303,7 +304,7 @@ minView (L _ x a b) = return (x, union a b)
 
 minElem :: Ord a => Heap a -> a
 minElem E = error "LeftistHeap.minElem: empty collection"
-minElem (L _ x a b) = x
+minElem (L _ x _ _) = x
 
 maxView :: (Ord a, Monad m) => Heap a -> m (a, Heap a)
 maxView E = fail "LeftistHeap.maxView: empty collection"
@@ -321,54 +322,54 @@ maxView (L _ x a b)
 maxElem :: Ord a => Heap a -> a
 maxElem E = error "LeftistHeap.maxElem: empty collection"
 maxElem (L _ x E _) = x
-maxElem (L _ x a b) = findMax b (findLeaf a)
+maxElem (L _ _ a b) = findMax b (findLeaf a)
   where findMax E m = m
         findMax (L _ x E _) m
           | m >= x = m
           | otherwise = x
-        findMax (L _ x a b) m = findMax a (findMax b m)
+        findMax (L _ _ d c) m = findMax d (findMax c m)
 
         findLeaf E = error "LeftistHeap.maxElem: bug"
         findLeaf (L _ x E _) = x
-        findLeaf (L _ x a b) = findMax b (findLeaf a)
+        findLeaf (L _ _ y c) = findMax c (findLeaf y)
 
 foldr :: Ord a => (a -> b -> b) -> b -> Heap a -> b
-foldr f e E = e
+foldr _ e E = e
 foldr f e (L _ x a b) = f x (foldr f e (union a b))
 
 foldr' :: Ord a => (a -> b -> b) -> b -> Heap a -> b
-foldr' f e E = e
+foldr' _ e E = e
 foldr' f e (L _ x a b) = e `seq` f x $! (foldr' f e (union a b))
 
 foldl :: Ord a => (b -> a -> b) -> b -> Heap a -> b
-foldl f e E = e
+foldl _ e E = e
 foldl f e (L _ x a b) = foldl f (f e x) (union a b)
 
 foldl' :: Ord a => (b -> a -> b) -> b -> Heap a -> b
-foldl' f e E = e
+foldl' _ e E = e
 foldl' f e (L _ x a b) = e `seq` foldl' f (f e x) (union a b)
 
 foldr1 :: Ord a => (a -> a -> a) -> Heap a -> a
-foldr1 f E = error "LeftistHeap.foldr1: empty collection"
-foldr1 f (L _ x E _) = x
+foldr1 _ E = error "LeftistHeap.foldr1: empty collection"
+foldr1 _ (L _ x E _) = x
 foldr1 f (L _ x a b) = f x (foldr1 f (union a b))
 
 foldr1' :: Ord a => (a -> a -> a) -> Heap a -> a
-foldr1' f E = error "LeftistHeap.foldr1': empty collection"
-foldr1' f (L _ x E _) = x
+foldr1' _ E = error "LeftistHeap.foldr1': empty collection"
+foldr1' _ (L _ x E _) = x
 foldr1' f (L _ x a b) = f x $! (foldr1' f (union a b))
 
 foldl1 :: Ord a => (a -> a -> a) -> Heap a -> a
-foldl1 f E = error "LeftistHeap.foldl1: empty collection"
+foldl1 _ E = error "LeftistHeap.foldl1: empty collection"
 foldl1 f (L _ x a b) = foldl f x (union a b)
 
 foldl1' :: Ord a => (a -> a -> a) -> Heap a -> a
-foldl1' f E = error "LeftistHeap.foldl1: empty collection"
+foldl1' _ E = error "LeftistHeap.foldl1: empty collection"
 foldl1' f (L _ x a b) = foldl' f x (union a b)
 
 {- ???? -}
 unsafeMapMonotonic :: Ord a => (a -> a) -> Heap a -> Heap a
-unsafeMapMonotonic f E = E
+unsafeMapMonotonic _ E = E
 unsafeMapMonotonic f (L i x a b) =
   L i (f x) (unsafeMapMonotonic f a) (unsafeMapMonotonic f b)
 
@@ -378,8 +379,8 @@ strict :: Heap a -> Heap a
 strict h = h
 
 strictWith :: (a -> b) -> Heap a -> Heap a
-strictWith f h@E = h
-strictWith f h@(L i x l r) = f x `seq` strictWith f l `seq` strictWith f r `seq` h
+strictWith _ h@E = h
+strictWith f h@(L _ x l r) = f x `seq` strictWith f l `seq` strictWith f r `seq` h
 
 -- the remaining functions all use default definitions
 
@@ -415,30 +416,30 @@ toOrdSeq = toOrdSeqUsingFoldr
 
 instance Ord a => C.CollX (Heap a) a where
   {singleton = singleton; fromSeq = fromSeq; insert = insert;
-   insertSeq = insertSeq; unionSeq = unionSeq; 
+   insertSeq = insertSeq; unionSeq = unionSeq;
    delete = delete; deleteAll = deleteAll; deleteSeq = deleteSeq;
    null = null; size = size; member = member; count = count;
    strict = strict;
-   structuralInvariant = structuralInvariant; instanceName c = moduleName}
+   structuralInvariant = structuralInvariant; instanceName _ = moduleName}
 
 instance Ord a => C.OrdCollX (Heap a) a where
-  {deleteMin = deleteMin; deleteMax = deleteMax; 
-   unsafeInsertMin = unsafeInsertMin; unsafeInsertMax = unsafeInsertMax; 
-   unsafeFromOrdSeq = unsafeFromOrdSeq; unsafeAppend = unsafeAppend; 
-   filterLT = filterLT; filterLE = filterLE; filterGT = filterGT; 
-   filterGE = filterGE; partitionLT_GE = partitionLT_GE; 
+  {deleteMin = deleteMin; deleteMax = deleteMax;
+   unsafeInsertMin = unsafeInsertMin; unsafeInsertMax = unsafeInsertMax;
+   unsafeFromOrdSeq = unsafeFromOrdSeq; unsafeAppend = unsafeAppend;
+   filterLT = filterLT; filterLE = filterLE; filterGT = filterGT;
+   filterGE = filterGE; partitionLT_GE = partitionLT_GE;
    partitionLE_GT = partitionLE_GT; partitionLT_GT = partitionLT_GT}
 
 instance Ord a => C.Coll (Heap a) a where
-  {toSeq = toSeq; lookup = lookup; lookupM = lookupM; 
-   lookupAll = lookupAll; lookupWithDefault = lookupWithDefault; 
+  {toSeq = toSeq; lookup = lookup; lookupM = lookupM;
+   lookupAll = lookupAll; lookupWithDefault = lookupWithDefault;
    fold = fold; fold' = fold'; fold1 = fold1; fold1' = fold1';
    filter = filter; partition = partition; strictWith = strictWith}
 
 instance Ord a => C.OrdColl (Heap a) a where
-  {minView = minView; minElem = minElem; maxView = maxView; 
+  {minView = minView; minElem = minElem; maxView = maxView;
    maxElem = maxElem; foldr = foldr; foldr' = foldr';
-   foldl = foldl; foldl' = foldl'; foldr1 = foldr1; 
+   foldl = foldl; foldl' = foldl'; foldr1 = foldr1;
    foldr1' = foldr1'; foldl1 = foldl1; foldl1' = foldl1';
    toOrdSeq = toOrdSeq; unsafeMapMonotonic = unsafeMapMonotonic}
 
@@ -467,7 +468,7 @@ instance (Ord a, Arbitrary a) => Arbitrary (Heap a) where
             | a == E || x <= minElem a = t
           sift (L r x (L r' y a b) E) =
                 L r y (sift (L r' x a b)) E
-          sift t@(L r x a b)
+          sift t@(L _ x a b)
             | x <= minElem a && x <= minElem b = t
           sift (L r x (L r' y a b) c)
             | y <= minElem c =
@@ -477,7 +478,7 @@ instance (Ord a, Arbitrary a) => Arbitrary (Heap a) where
           sift _ = error "LeftistHeap.arbitrary: bug!"
 
   coarbitrary E = variant 0
-  coarbitrary (L _ x a b) = 
+  coarbitrary (L _ x a b) =
       variant 1 . coarbitrary x . coarbitrary a . coarbitrary b
 
 instance (Ord a) => Monoid (Heap a) where
